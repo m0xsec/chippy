96085459265bfc03ad2bfb9d7d57d41f6b676596
m0x <m0x@m0x.io>
2021-11-16 18:09:53 -0800

    Mapped out where all CHIP-8 instructions should go and what needs to be implemented

--- pkg/chip8/chip8.go
+++ pkg/chip8/chip8.go
@@ -195,9 +195,11 @@ func (c *Chip8) Cycle() {
 	// Decode & Execute Opcode
 	// Ex: 0xA2F0 & 0xF000 -> 0xA000
 	switch c.oc & 0xF000 {
+	/////////////////////////////////////////////////////////////////////////////////////////
 	// Instrucutions starting with 0x0
 	// 0x00E0 - Clear the display
 	// 0x00EE - Return from a subroutine
+	// NOTE: Did not implement 0x0NNN - Used for running machine language outside of CHIP-8
 	case 0x0000:
 		// Need to compare the last 4 bits of the opcode
 		// Ex: 0x000E & 0x000F-> 0x000E
@@ -222,9 +224,15 @@ func (c *Chip8) Cycle() {
 			fmt.Printf("[0x0000] Unknown opcode: 0x%X\n", c.oc)
 		}
 
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x1
+	// 0x1NNN - Jump to address NNN
 	case 0x1000: // 0x1NNN Jump to address NNN
 		c.pc = c.oc & 0x0FFF
 
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x2
+	// 0x2NNN - Call subroutine at NNN
 	case 0x2000: // 0x2NNN Call subroutine at NNN
 		// Store the current PC on the stack,
 		// and increase stack pointer since we put something on the stack
@@ -233,19 +241,82 @@ func (c *Chip8) Cycle() {
 		c.sp += 1
 		c.pc = c.oc & 0x0FFF
 
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x3
+	// 0x3XNN - Skip next instruction if VX equals NN
+	// TODO: Implement 0x03 instructions
+	// ...
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x4
+	// 0x4XNN - Skip next instruction if VX doesn't equal NN
+	// TODO: Implement 0x4 instructions
+	// ...
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x5
+	// 0x5XY0 - Skip next instruction if VX equals VY
+	// TODO: Implement 0x5 instructions
+	// ...
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x6
+	// 0x6XNN - Set Register VX to NN
 	case 0x6000: // 0x6XNN Set Register VX to NN
 		// Need to shift right 8 bytes to get X
 		c.v[(c.oc&0x0F00)>>8] = uint8(c.oc & 0x00FF)
 		c.pc += 2
 
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x7
+	// 0x7XNN - Add NN to Register VX
 	case 0x7000: // 0x7XNN Add NN to Register VX
 		c.v[(c.oc&0x0F00)>>8] += uint8(c.oc & 0x00FF)
 		c.pc += 2
 
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x8
+	// 0x8XY0 - Set Register VX to Register VY
+	// 0x8XY1 - Set Register VX to Register VX OR Register VY
+	// 0x8XY2 - Set Register VX to Register VX AND Register VY
+	// 0x8XY3 - Set Register VX to Register VX XOR Register VY
+	// 0x8XY4 - Add Register VY to Register VX, set VF to 1 if carry, 0 if not
+	// 0x8XY5 - Subtract Register VY from Register VX, set VF to 0 if borrow, 1 if not
+	// 0x8XY6 - Store the least significant bit of Register VX in VF, and then shift Register VX right by 1
+	// 0x8XY7 - Set Register VX to Register VY minus Register VX, set VF to 0 if borrow, 1 if not
+	// 0x8XYE - Store the most significant bit of Register VX in VF, and then shift Register VX left by 1
+	// TODO: Implement 0x8 instructions
+	// ...
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0x9
+	// 0x9XY0 - Skip next instruction if VX doesn't equal VY
+	// TODO: Implement 0x9 instructions
+	// ...
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0xA
+	// 0xANNN - Set I to NNN
 	case 0xA000: // 0xANNN Set I to NNN
 		c.i = c.oc & 0x0FFF
 		c.pc += 2
 
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0xB
+	// 0xBNNN - Jump to address NNN + V0
+	// TODO: Implement 0xB instructions
+	// ...
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0xC
+	// 0xCXNN - Set VX to a random number and NN
+	// TODO: Implement 0xC instructions
+	// ...
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0xD
+	// 0xDXYN - Draw a sprite at position VX, VY with N bytes of sprite data starting at the address
+	//			stored in I. Set VF to 01 if any set pixels are changed to unset, and 00 otherwise
 	case 0xD000: // 0xDXYN Display (Drawing)
 		// Fetch (X,Y) from VX and VY
 		x := c.v[(c.oc&0x0F00)>>8] % uint8(DISPLAY_WIDTH)
@@ -282,7 +353,29 @@ func (c *Chip8) Cycle() {
 		fmt.Printf("[0xDXYN] X: %d, Y: %d, N: %d\n", x, y, n)
 		c.pc += 2
 
-	// TODO: Implement remaining CHIP-8 insturctions
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0xE
+	// 0xEX9E - Skip next instruction if key stored in VX is pressed
+	// 0xEXA1 - Skip next instruction if key stored in VX isn't pressed
+	// TODO: Implement 0xE instructions
+	// ...
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Instrucutions starting with 0xF
+	// 0xFX07 - Set VX to the value of the delay timer
+	// 0xFX0A - Wait for a key press, store the value of the key in VX
+	// 0xFX15 - Set the delay timer to VX
+	// 0xFX18 - Set the sound timer to VX
+	// 0xFX1E - Add VX to I
+	// 0xFX29 - Set I to the location of the sprite for the character in VX
+	// 0xFX33 - Store the binary-coded decimal representation of VX in memory locations
+	//			I, I+1, and I+2
+	// 0xFX55 - Store the values of registers V0 to VX inclusive in memory starting at address I
+	//			I is set to I + X + 1 after operation
+	// 0xFX65 - Fill registers V0 to VX inclusive with the values stored in memory starting at address I
+	//			I is set to I + X + 1 after operation
+	// TODO: Implement 0xF instructions
+	// ...
 
 	default:
 		fmt.Printf("Unknown opcode: 0x%X\n", c.oc)
